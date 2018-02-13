cd9b5b8a202494abe6a609fb68d127ce3ea886aa
Jan Skoula <skoula.jan@gmail.com>
2018-02-13 17:27:30 +0100

    made changes

--- nim.js
+++ nim.js
@@ -1,4 +1,21 @@
 sirky = Math.random(20,30);
-var obtiznosti  = ["easy", "normal", "hard"];
+var obtiznosti  = ["lehká", "střední", "těžká"];
 var obtiznost=0;
 
+function obtiznostHry(){
+	obtiznost += 1;
+	if (obtiznost > 2){
+	obtiznost = 0;
+	}
+	return "Obtížnost: " + obtiznosti[obtiznost];
+}
+document.getElementById("pravidla").style.display = "none";
+function pravidla(){
+	var x = document.getElementById("pravidla");
+	if (x.style.display== "none") {
+		x.style.display = "block";
+	}
+	else{
+		x.style.display = "none";
+	}
+}
